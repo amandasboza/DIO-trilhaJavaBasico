ab37e42d532b273f7b7c689fce628dcb5c49180c
amandasboza <nandasboza@gmail.com>
2023-05-24 06:28:59 -0300

    Anotações sobre operadores e métodos

---
+++ tipos-variaveis/src/Metodo.java
@@ -0,0 +1,22 @@
+public class Metodo {
+    
+   // Critério de nomeação de Métodos
+   
+        // Esses critérios não são obrigatórios, mas é recomendável que sejam seguidos, pois essas convenções facilitam a vida dos programadores ao trabalharem em códigos de forma colaborativa. Ao seguir estas convenções, tornamos o código mais legível para nós e também para outras pessoas. Para métodos, os critérios são:
+            // Deve ser nomeado como verbo;
+            // Seguir o padrão camelCase (Todas as letras minúsculas com a exceção da primeira letra da segunda palavra).
+
+           /*  
+           
+           somar(int n1, int n2){}
+
+            abrirConexao(){}
+
+            concluirProcessamento() {}
+
+            findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+
+            calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+
+            */
+}
--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -80,7 +80,28 @@ public class Operadores {
         //  < Quando desejamos verificar se uma variável é MENOR QUE outra.
         //  <= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
 
+    // Lógicos
+        // Os operadores lógicos, representam o recurso que nos permite criar expressões lógicas maiores, a partir da junção de duas ou mais expressões.
+            // && Operador Lógico "E"
+            // || Operador Lógico "OU"
 
+        // Operadores.java
+        
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
 }       
     }
 	
